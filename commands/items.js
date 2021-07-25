7c96d748cb7feae97cba4893962c3fcfd1ed9d0a
dm94 <dm94@hotmail.es>
2021-07-25 09:19:39 +0200

    2.0.5: Now it does not make a request every time it checks the items

--- commands/items.js
+++ commands/items.js
@@ -3,6 +3,8 @@ const itemsCommands = {};
 const Discord = require("discord.js");
 const Axios = require("axios");
 const othersFunctions = require("../helpers/others");
+let itemsLastCheck = 0;
+let allItems = null;
 
 itemsCommands.locraft = (msg, args, prefix) => {
   console.log(new Date() + " " + msg.content);
@@ -28,58 +30,59 @@ itemsCommands.locraft = (msg, args, prefix) => {
   }
 };
 
-function getNecessaryMaterials(itemName, msg, multiplier) {
+async function getNecessaryMaterials(itemName, msg, multiplier) {
   if (itemName.length <= 0) {
     return;
   }
   var message = multiplier + "x " + itemName;
   let itemsSent = 0;
 
-  Axios.get(
-    "https://raw.githubusercontent.com/dm94/stiletto-web/master/public/json/items_min.json"
-  )
-    .then((response) => {
-      let items = response.data;
-
-      if (items != null) {
-        let itemsfilters = items.filter((item) =>
-          item.name.toLowerCase().includes(itemName)
-        );
-        itemsfilters.forEach((item) => {
-          message = new Discord.MessageEmbed()
-            .setColor("#FFE400")
-            .setTitle(multiplier + "x " + item.name)
-            .setDescription("Here are the necessary materials");
-          let ingredie = item.crafting;
-          if (ingredie != null) {
-            for (var i = 0; i < ingredie.length; i++) {
-              let le = ingredie[i].ingredients;
-              for (var ing in le) {
-                areItems = true;
-                message.addField(
-                  le[ing].name,
-                  le[ing].count * multiplier,
-                  true
-                );
-              }
-            }
-          }
-          if (item.cost != null) {
-            message.setFooter(
-              "Cost: " + item.cost.count + " " + item.cost.name
-            );
-          }
-
-          if (itemsSent < 5) {
-            itemsSent++;
-            othersFunctions.sendChannelMessage(msg, message);
-          }
-        });
+  const items = await getAllItems();
+  let itemsfilters = items.filter((item) =>
+    item.name.toLowerCase().includes(itemName)
+  );
+  itemsfilters.forEach((item) => {
+    message = new Discord.MessageEmbed()
+      .setColor("#FFE400")
+      .setTitle(multiplier + "x " + item.name)
+      .setDescription("Here are the necessary materials");
+    let ingredie = item.crafting;
+    if (ingredie != null) {
+      for (var i = 0; i < ingredie.length; i++) {
+        let le = ingredie[i].ingredients;
+        for (var ing in le) {
+          areItems = true;
+          message.addField(le[ing].name, le[ing].count * multiplier, true);
+        }
       }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    }
+    if (item.cost != null) {
+      message.setFooter("Cost: " + item.cost.count + " " + item.cost.name);
+    }
+
+    if (itemsSent < 5) {
+      itemsSent++;
+      othersFunctions.sendChannelMessage(msg, message);
+    }
+  });
+}
+
+function getAllItems() {
+  if (allItems != null && itemsLastCheck >= Date.now() - 3600000) {
+    return allItems;
+  } else {
+    return Axios.get(
+      "https://raw.githubusercontent.com/dm94/stiletto-web/master/public/json/items_min.json"
+    )
+      .then((response) => {
+        allItems = response.data;
+        itemsLastCheck = Date.now();
+        return allItems;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
 
 module.exports = itemsCommands;
