529bf8db0f10811cdb39f73bea3d493b616f2d38
dm94 <dm94@hotmail.es>
2021-11-28 15:52:27 +0100

    3.2.1: Now shows the object's icon and a link to view its data.

--- commands/items.js
+++ commands/items.js
@@ -89,10 +89,52 @@ itemsCommands.getNecessaryMaterials = async (channel, itemName, multiplier) => {
 };
 
 function setItemInfo(channel, item, multiplier) {
+  let name = item.name;
+  if (name.includes("Tier 1")) {
+    name = "Walker Upgrade Wood";
+  } else if (name.includes("Tier 2")) {
+    name = "Walker Upgrade Bone";
+  } else if (name.includes("Tier 3")) {
+    name = "Walker Upgrade Ceramic";
+  } else if (name.includes("Tier 4")) {
+    name = "Walker Upgrade Iron";
+  } else if (name.includes("Wings Small")) {
+    name = "Walker Wings Small";
+  } else if (name.includes("Wings Medium")) {
+    name = "Walker Wings Medium";
+  } else if (name.includes("Wings Large")) {
+    name = "Walker Wings Large";
+  } else if (name.includes("Wings Skirmish")) {
+    name = "Walker Wings Skirmish";
+  } else if (name.includes("Wings Raider")) {
+    name = "Walker Wings Raider";
+  } else if (name.includes("Wings Heavy")) {
+    name = "Walker Wings Heavy";
+  } else if (name.includes("Wings Rugged")) {
+    name = "Walker Wings Rugged";
+  } else if (name.includes(" Wings")) {
+    name = "Walker Wings";
+  } else if (name.includes("Legs Armored")) {
+    name = "Walker Legs Armored";
+  } else if (name.includes("Legs Heavy")) {
+    name = "Walker Legs Heavy";
+  } else if (name.includes("Legs")) {
+    name = "Walker Legs";
+  } else if (name.includes("Grappling Hook")) {
+    name = "Grappling Hook";
+  }
+  name = name.replaceAll("Body", "");
+
   let message = new Discord.MessageEmbed()
     .setColor("#FFE400")
     .setTitle(multiplier + "x " + item.name)
-    .setDescription("Here are the necessary materials");
+    .setDescription("Here are the necessary materials")
+    .setURL(
+      "https://www.stiletto.live/item/" + encodeURI(item.name.toLowerCase())
+    )
+    .setThumbnail(
+      "https://api.stiletto.live/items/" + encodeURI(name.trim() + " icon.png")
+    );
   let ingredie = item.crafting;
   if (ingredie != null) {
     for (var i = 0; i < ingredie.length; i++) {
