faa8693094b9467ebd33693d2057f53b379ec813
dm94 <dm94@hotmail.es>
2022-03-01 00:32:19 +0100

    Fail

--- helpers/discordlists.js
+++ helpers/discordlists.js
@@ -4,6 +4,7 @@ const logger = require("./logger");
 
 controller.updateStatistics = async (guildsCount, usersCount) => {
   controller.sendDiscordBotList(guildsCount, usersCount);
+  controller.sendTopGG(guildsCount);
 };
 
 /**
