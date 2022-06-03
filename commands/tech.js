ac45bbb2b6397fc752d52772f5fca9cf3f1cf464
dm94 <dm94@hotmail.es>
2022-06-03 17:07:45 +0200

    v3.4.23 Fix some errors

--- commands/tech.js
+++ commands/tech.js
@@ -12,51 +12,49 @@ commands.getWhoHasLearntIt = async (interaction) => {
   let item = await itemsFunctions.getItem(itemName);
 
   if (item != null) {
-    let tree = itemsFunctions.getTechTree(item.name);
-    if (tree != null) {
-      const options = {
-        method: "get",
-        url: process.env.APP_API_URL + "/bot/" + discordid + "/tech",
-        params: {
-          tree: tree,
-          tech: item.name,
-        },
-      };
+    let tree = await itemsFunctions.getTechTree(item.name);
+    const options = {
+      method: "get",
+      url: process.env.APP_API_URL + "/bot/" + discordid + "/tech",
+      params: {
+        tree: tree,
+        tech: item.name,
+      },
+    };
 
-      let response = await othersFunctions.apiRequest(options);
-      if (response.success) {
-        if (Array.isArray(response.data) && response.data.length > 0) {
-          let message = new Discord.MessageEmbed()
-            .setColor("#3A78EA")
-            .setTitle(item.name)
-            .setDescription("These are the people who have learned it")
-            .setURL(
-              "https://www.stiletto.live/item/" +
-                encodeURI(item.name.toLowerCase())
-            );
-          let respondeLenght = response.data.length;
-          for (let i = 0; i < respondeLenght; i++) {
-            if (response.data[i].discordtag != null) {
-              message.addField("Discord", response.data[i].discordtag, false);
-            }
+    let response = await othersFunctions.apiRequest(options);
+    if (response.success) {
+      if (Array.isArray(response.data) && response.data.length > 0) {
+        let message = new Discord.MessageEmbed()
+          .setColor("#3A78EA")
+          .setTitle(item.name)
+          .setDescription("These are the people who have learned it")
+          .setURL(
+            "https://www.stiletto.live/item/" +
+              encodeURI(item.name.toLowerCase())
+          );
+        let respondeLenght = response.data.length;
+        for (let i = 0; i < respondeLenght; i++) {
+          if (response.data[i].discordtag != null) {
+            message.addField("Discord", response.data[i].discordtag, false);
           }
-          await interaction.editReply({
-            content: message,
-            ephemeral: true,
-          });
-        } else {
-          await interaction.editReply({
-            content: "There is no one from your clan with this learnt",
-            ephemeral: true,
-          });
         }
+        await interaction.editReply({
+          content: message,
+          ephemeral: true,
+        });
       } else {
         await interaction.editReply({
-          content:
-            "For this command to work the clan must be linked to this discord.",
+          content: "There is no one from your clan with this learnt",
           ephemeral: true,
         });
       }
+    } else {
+      await interaction.editReply({
+        content:
+          "For this command to work the clan must be linked to this discord.",
+        ephemeral: true,
+      });
     }
   } else {
     await interaction.editReply({
@@ -74,29 +72,27 @@ commands.addTech = async (interaction) => {
   let item = await itemsFunctions.getItem(itemName);
 
   if (item != null) {
-    let tree = itemsFunctions.getTechTree(item.name);
-    if (tree != null) {
-      const options = {
-        method: "post",
-        url: process.env.APP_API_URL + "/bot/" + discordid + "/tech",
-        params: {
-          tree: tree,
-          tech: item.name,
-        },
-      };
+    let tree = await itemsFunctions.getTechTree(item.name);
+    const options = {
+      method: "post",
+      url: process.env.APP_API_URL + "/bot/" + discordid + "/tech",
+      params: {
+        tree: tree,
+        tech: item.name,
+      },
+    };
 
-      let response = await othersFunctions.apiRequest(options);
-      if (response.success) {
-        await interaction.editReply({
-          content: "Added to the technology tree: " + item.name,
-          ephemeral: true,
-        });
-      } else {
-        await interaction.editReply({
-          content: response.data,
-          ephemeral: true,
-        });
-      }
+    let response = await othersFunctions.apiRequest(options);
+    if (response.success) {
+      await interaction.editReply({
+        content: "Added to the technology tree: " + item.name,
+        ephemeral: true,
+      });
+    } else {
+      await interaction.editReply({
+        content: response.data,
+        ephemeral: true,
+      });
     }
   } else {
     await interaction.editReply({
